16f3c5d150be38d518c048ed18dad387fd4f412a
Geoffrey Bascour <geoffreybascour93@gmail.comm>
2019-04-09 17:00:00 +0200

    react

---
+++ src/Travel.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+const Travel = () => (
+<figure>
+    <img
+      src="https://image.noelshack.com/fichiers/2019/15/2/1554797978-852192.jpg"
+      alt="Japon"
+    />
+    <figcaption>
+        <nav>
+          
+       <li> Destination : Tokyo</li>
+      
+
+      <li>Country : Japan</li>
+
+      
+      <li>Distance : 9 442 km</li>
+       
+      </nav>
+
+
+
+<img
+      src="https://image.noelshack.com/fichiers/2019/15/2/1554797804-bali-temples-jpg.jpg"
+      alt="Bali"
+    />
+    <nav>
+          
+       <li> Destination : Bali</li>
+      
+
+      <li>Country : Indonesia</li>
+
+      
+      <li>Distance : 12 215 km</li>
+       
+      </nav>
+      </figcaption>
+  </figure>
+);
+export default Travel;
